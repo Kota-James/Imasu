c769956b55a703922c2bfa8d9831313df759d4e3
Kota-James <n.kota.all.01@gmail.com>
2026-02-11 12:46:09 +0900

    DB再設計とschemas作成

--- backend/models.py
+++ backend/models.py
@@ -14,9 +14,13 @@ class User(_database.Base):
     display_name = _sql.Column(_sql.String, index=True) #表示名
 
     role = _sql.Column(_sql.String, default="user") # 権限: "master", "admin", "user"
+    status = _sql.Column(_sql.String, default="out", index=True) # 状態管理 "in", "out", "away"
     color_code = _sql.Column(_sql.String, default="#3b82f6") # UI用: テーマカラー (デフォルトは青色 #3b82f6)
     nfc_card_id = _sql.Column(_sql.String, unique=True, index=True, nullable=True) # 学生証連携 (FeliCa IDmなど) - 登録しない人もいるので nullable=True
     is_deleted = _sql.Column(_sql.Boolean, default=False) # 論理削除フラグ: Trueなら削除済み扱い
+
+    date_created = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now())
+    date_last_updated = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     logs = _orm.relationship("Log", back_populates="owner")
 
@@ -27,10 +31,10 @@ class Log(_database.Base):
     __tablename__ = "logs"
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id")) #誰のログか
-    status = _sql.Column(_sql.Integer, index=True) #入退室等の状態管理
+    action = _sql.Column(_sql.String, index=True)   # "Enter", "exit", "go_out", "return"
     place = _sql.Column(_sql.String, index=True, default="") #場所
     note = _sql.Column(_sql.String, default="")
-    date_created = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now())
-    date_last_updated = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    date_created_log = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now())
+    date_last_updated_log = _sql.Column(_sql.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     owner = _orm.relationship("User", back_populates="logs")
---
+++ backend/schemas.py
@@ -0,0 +1,37 @@
+import pydantic as _pydantic
+import datetime as _dt
+
+### Base Schemas
+class _UserBase(_pydantic.BaseModel):
+    email: _pydantic.EmailStr
+
+### Input Schemas
+class UserCreate(_UserBase):    # creating account
+    password: str
+
+class UserBaseUpdate(_pydantic.BaseModel):  # registering prifile, and another info
+    display_name: str | None = None
+    original_id: str | None = None
+    role: str | None = None
+    color_code: str | None = None
+    nfc_card_id: str | None = None
+    is_deleted: bool | None = None
+
+### Output Schemas
+class User(_UserBase):  # output
+    id: int
+
+    # profile
+    display_name: str | None = None
+    original_id: str | None = None
+    role: str
+    color_code: str
+    nfc_card_id: str | None = None
+
+    # system
+    status: bool
+    is_deleted: bool
+    date_created: _dt.datetime
+    date_last_updated: _dt.datetime
+    
+    model_config = _pydantic.ConfigDict(from_attributes=True)
